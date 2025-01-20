f0c9f8b38c7b3374b01e22e5be4300933c8be9dd
KaziTanvir <kazitawhidibnmonir@gmail.com>
2025-01-20 13:02:48 +0600

    Module7 class_3- elevated button and icon use

---
+++ lib/M7class_3/M7class_3.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Center(
+          child: Text(
+            "Ostad App",
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5))),
+                onPressed: () {
+                  print("Button pressd");
+                },
+                child: Text(
+                  "Button",
+                  style: TextStyle(color: Colors.white,fontSize: 25),
+                )),
+          ),
+          Icon(Icons.person,size: 25,color: Colors.cyan,)
+        ],
+      ),
+    ),
+  ));
+}
